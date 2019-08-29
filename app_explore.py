55d2d93a286c9880e2efbe353b94c13fe79423bd
Ipshita <ipshitarb1707@gmail.com>
2019-08-29 17:24:28 -0400

    Open and load Android file in to a list

--- app_explore.py
+++ app_explore.py
@@ -4,3 +4,8 @@ open_apple_file = open('AppleStore.csv')
 apple_content = reader(open_apple_file)
 ios_app_data = list(apple_content)
 
+# open and load google app contents in to a list
+open_google_file = open('googleplaystore.csv', encoding='utf8')
+android_content = reader(open_google_file)
+android_app_data = list(android_content)
+
