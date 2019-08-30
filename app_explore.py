a7c43c3685f1420dd2090bbf737272db4130632b
Ipshita <ipshitarb1707@gmail.com>
2019-08-30 14:28:59 -0400

    Explore iOS and Android data using function
    - to take a look at the rows and columns and values of the iOS and Android dataset

--- app_explore.py
+++ app_explore.py
@@ -9,3 +9,15 @@ open_google_file = open('googleplaystore.csv', encoding='utf8')
 android_content = reader(open_google_file)
 android_app_data = list(android_content)
 
+def explore_data(dataset, start, end, rows_and_columns=False):
+    dataset_slice = dataset[start:end]    
+    for row in dataset_slice:
+        print(row)
+        print('\n') # adds a new (empty) line after each row
+
+    if rows_and_columns:
+        print('Number of rows:', len(dataset))
+        print('Number of columns:', len(dataset[0]))
+        
+explore_data(ios_app_data, 0, 5)
+explore_data(android_app_data, 0, 5)
